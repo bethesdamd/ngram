973ca6e25c52357812c539647927097ce5e9d1ab
David Swearingen <swearingendw@mac.com>
2014-07-16 21:31:39 -0400

    first final version

--- parse.rb
+++ parse.rb
@@ -1,9 +1,15 @@
 #  parse.rb
+# Parses n-grams from a document, contained in String
+# Variable 'max' specifies largest n-gram size, e.g. specifying max = 3 means create 1, 2 and 3-grams
+# Assumptions:  
+# 	- The documents are not enormous (but they certainly could be hundreds of pages.)
 
 max = 3
 
+# The document is stored in String s
 s = "one two three four five"
 
+# Ngram class holds the ngram text in String @text, and the size of the ngram in @size
 class Ngram
 	def initialize(text, size)
 		@text = text
@@ -13,23 +19,33 @@ class Ngram
 end
 
 f = []
-s.split().each do |t|
+# Split the document on white space.  Clean up the terms. Then create an array containing every word in the document.
+s.split().each do |t|   
 	# remove punctuation
 	# remove noise words
 	f.push(t.strip)
 end
 
+# arr_ngram_objects will be an array holding all the ngrams
 arr_ngram_objects = []
 (1..max).each do |ngsize|  # for each n-gram size, e.g. the 1-grams, 2-grams and 3-grams
-	(0..f.size-ngsize).each do |term|  # for each term in the list  0-4
+	(0..f.size-ngsize).each do |term|  # for each term in the list
 		temp = []
-		(0..ngsize-1).each do |word|   # 0-3
+		(0..ngsize-1).each do |word|   # e.g. for a 2-gram, loop twice to grab two terms
 			temp.push(f[term + word])
 		end
-		puts "ngram: " + temp.inspect
 		ngram_obj = Ngram.new(temp.join(" "), ngsize)
 		arr_ngram_objects.push ngram_obj
 	end
 end
 
-puts arr_ngram_objects.inspect
+arr_ngram_objects.sort! { |a,b| a.size <=> b.size }
+arr_ngram_objects.each do |n| puts n.text + " " + n.size.to_s end
+
+arr_ngram_objects.sort! { |a,b| a.text <=> b.text }
+arr_ngram_objects.each do |n| puts n.text + " " + n.size.to_s end
+
+
+
+
+
